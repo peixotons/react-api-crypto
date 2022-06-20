5fa6160f542d96228abc818f6bf8ebf6c780058e
Gabriel Peixoto <bielpcosta@hotmail.com>
2022-06-20 13:50:39 -0300

    primeiro commit

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import './App.css';
+import Coin from './Coin';
+
+function App() {
+  const [coins, setCoins] = useState([]);
+  const [search, setSearch] = useState('');
+
+   useEffect(() => {
+    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=brl&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+    .then(res => {
+      setCoins(res.data)
+    })
+    .catch(error => console.log(error))
+   }, []);
+
+   const handleChange = e => {
+    setSearch(e.target.value)
+   }
+
+   const filteredCoins = coins.filter(coin=>
+    coin.name.toLowerCase().includes(search.toLowerCase())
+    )
+
+  return (
+    <div className='coin-app'>
+      <div className='coin-search'>
+        <h1 className='coin-text'>Pesquise uma Moeda</h1>
+        <form>
+        <input type="text" placeholder="Pesquisar" className="coin-input" onChange={handleChange}/>
+        </form>
+      </div>  
+      {filteredCoins.map(coin => {
+        return <Coin 
+          key={coin.id} 
+          name={coin.name} 
+          image={coin.image}
+          symbol={coin.symbol}
+          volume={coin.total_volume}
+          price={coin.current_price}
+          priceChange={coin.price_change_percentage_24h}
+          marketcap={coin.market_cap}
+          />;        
+      })}
+    </div>
+  );
+}
+
+export default App;
